6d554e0ddb2aed8564cd704cfd78c1b29f168bff
NamelessOgya <hakuginnnohana@gmail.com>
2024-11-02 02:42:16 +0000

    update notebook

--- src/bitnet_cpp/llm_module.py
+++ src/bitnet_cpp/llm_module.py
@@ -3,52 +3,71 @@ sys.path.append('/content/tmp/BitNet')
 
 import subprocess
 
+def run_command(command):
+    try:
+      process = subprocess.Popen(
+          command,
+          stdout=subprocess.PIPE,
+          stderr=subprocess.PIPE,
+          text=True,
+          cwd="./tmp/BitNet"
+      )
+      
+      output = []  # 標準出力を格納するリスト
+      # 出力をリアルタイムで表示
+      for line in process.stdout:
+          print(line, end='')  # ノートブックに出力
+          output.append(line)  # 出力をリストに追加
 
+      # エラー出力を表示
+      stderr = process.stderr.read()
+      if stderr:
+          print(stderr)
+
+      process.wait()  # プロセスが終了するのを待つ
+    except subprocess.CalledProcessError as e:
+      print("エラーが発生しました。")
+    return process.returncode, ''.join(output)  # 戻り値をタプルとして返す
 
 class LLMModule:
   def __init__(self, repo_id, filename):
     print("llm module of bitnet")
-    
     try:
-      result = subprocess.run(
+      result_code, result_output = run_command(
         [
           'python', 
-          './BitNetsetup.py', 
+          'setup_env.py', 
           '--hf-repo', 
           repo_id, 
           '--quant-type', 
           filename
-        ], 
-        check=True,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-        text=True  # Python 3.7以上の場合。出力を文字列として取得
+        ]
       )
-      
-      print("標準出力:\n", result.stdout)
+      print("終了コード:", result_code)
+      print("標準出力:\n", result_output)  # ここで標準出力を表示
     except subprocess.CalledProcessError as e:
       print("エラーが発生しました。")
-      print("リターンコード:", e.returncode)
-      print("標準出力:\n", e.stdout)
-      print("標準エラー出力:\n", e.stderr)
+
+    self.repo_id = repo_id
+    self.filename = filename
 
   def infer(self, prompt):
-    args = [
-      "-m", 
-      "models/" + "a", 
-      "-q", 
-      "i2_s"
-    ]
-    result = subprocess.run(
+    result_code, result_output = run_command(
       [
-        "python", 
-        "/content/tmp/BitNet/run_inference.py"
+        "python",
+        "run_inference.py",
+        "-m",
+        f"models/{self.repo_id.split('/')[1]}/ggml-model-{self.filename}.gguf",#todo: 後で自動生成
+        "-p",
+        prompt,
+        "-n", 
+        "30", 
+        "-temp",
+        "0"
       ]
-      , capture_output=True
-      , text=True
     )
 
-    return result
+    return result_output
 
 
 # python run_inference.py -m models/Llama3-8B-1.58-100B-tokens/ggml-model-i2_s.gguf -p "Daniel went back to the the the garden. Mary travelled to the kitchen. Sandra journeyed to the kitchen. Sandra went to the hallway. John went to the bedroom. Mary went back to the garden. Where is Mary?\nAnswer:" -n 6 -temp 0
