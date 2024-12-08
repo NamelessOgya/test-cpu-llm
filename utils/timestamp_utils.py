992caa22f43411007d610f8ce0101fb0ad11a68a
NamelessOgya <hakuginnnohana@gmail.com>
2024-12-08 03:41:59 +0000

    enhance prompt. implements chat inference

---
+++ utils/timestamp_utils.py
@@ -0,0 +1,16 @@
+from datetime import datetime, timezone, timedelta
+
+
+def get_date_str():
+  # JST（UTC+9）タイムゾーンを設定
+  jst = timezone(timedelta(hours=9))
+
+  # 現在のJSTの日時を取得
+  current_time_jst = datetime.now(jst)
+
+  # 日付と時間を文字列で取得
+  date_str = current_time_jst.strftime("%Y%m%d%H%M%S")
+
+  return date_str
+
+if 
